dbba6df1457df92b280cfed16596a6788b8e348c
rajraebl <rajraebl@gmail.com>
2020-01-22 16:17:49 +0530

    connectionDone

--- notebooks/note1.scala
+++ notebooks/note1.scala
@@ -3,7 +3,7 @@ val configs = Map(
   "fs.azure.account.auth.type" -> "OAuth",
   "fs.azure.account.oauth.provider.type" -> "org.apache.hadoop.fs.azurebfs.oauth2.ClientCredsTokenProvider",
   "fs.azure.account.oauth2.client.id" -> "9cc631de-cb6a-4910-9b94-1fd8c49014fa",
-  "fs.azure.account.oauth2.client.secret" -> dbutils.secrets.get(scope = "training-scope", key = "dlstoken"),
+  "fs.azure.account.oauth2.client.secret" -> dbutils.secrets.get(scope = "adbtraining-scope", key = "dlstoken"),
   "fs.azure.account.oauth2.client.endpoint" -> "https://login.microsoftonline.com/72f988bf-86f1-41af-91ab-2d7cd011db47/oauth2/token")
 
 
